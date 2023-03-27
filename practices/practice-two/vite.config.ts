469c2416fbca907e44ced9b3c333ce20f1401d80
duong.pham <duong.pham@asnet.com.vn>
2023-03-27 16:40:21 +0700

    FIX: #33 Create eviroment file

--- practices/practice-two/vite.config.ts
+++ practices/practice-two/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import environmentPlugin from 'vite-plugin-environment';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    environmentPlugin({
+      VITE_URL: process.env.VITE_URL,
+      VITE_RECOMMENDED_URL: process.env.VITE_RECOMMENDED_URL,
+    }),
+  ],
 });
