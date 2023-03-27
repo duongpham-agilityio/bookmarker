54f76812c2c87ca12a25a8aabc6bc39fda7d3dd4
duong.pham <duong.pham@asnet.com.vn>
2023-03-27 16:29:03 +0700

    FIX: #33 Update config storybook

--- practices/practice-two/.storybook/main.cjs
+++ practices/practice-two/.storybook/main.cjs
@@ -1,15 +1,45 @@
-module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+const path = require('path');
+const { mergeConfig } = require('vite');
+
+const config = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-actions',
   ],
-  framework: "@storybook/react",
+  framework: '@storybook/react',
   core: {
-    builder: "@storybook/builder-vite",
+    builder: '@storybook/builder-vite',
   },
   features: {
     storyStoreV7: true,
   },
+  async viteFinal(config, { configType }) {
+    if (configType === 'DEVELOPMENT') {
+      return mergeConfig(config, {
+        resolve: {
+          alias: {
+            '@constants': path.resolve(__dirname, '../src/constants'),
+            components: path.resolve(__dirname, '../src/components'),
+            assets: path.resolve(__dirname, '../src/assets'),
+            helpers: path.resolve(__dirname, '../src/helpers'),
+            contexts: path.resolve(__dirname, '../src/contexts'),
+            hooks: path.resolve(__dirname, '../src/hooks'),
+            layouts: path.resolve(__dirname, '../src/layouts'),
+            pages: path.resolve(__dirname, '../src/reducers'),
+            reducers: path.resolve(__dirname, '../src/reducers'),
+            routes: path.resolve(__dirname, '../src/routes'),
+            styles: path.resolve(__dirname, '../src/styles'),
+            types: path.resolve(__dirname, '../src/types'),
+            'mock-data': path.resolve(__dirname, '../src/mock-data'),
+          },
+        },
+      });
+    }
+    return config;
+  },
 };
+
+module.exports = config;
