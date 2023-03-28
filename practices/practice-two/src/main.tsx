14f425798c3c84795229d1c51f83a37a64f4f176
duong.pham <duong.pham@asnet.com.vn>
2023-03-28 11:27:30 +0700

    FIX: #33 Install jest-dom and add style global

--- practices/practice-two/src/main.tsx
+++ practices/practice-two/src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// Styles
+import 'styles/index.module.css';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode></React.StrictMode>
 );
