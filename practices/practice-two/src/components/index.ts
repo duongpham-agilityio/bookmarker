4d3b33eab3c8f4617cc77445eaa957ea4f7b30c2
duong.pham <duong.pham@asnet.com.vn>
2023-03-28 17:43:08 +0700

    FIX: #36 Create Notification component

---
+++ practices/practice-two/src/components/index.ts
@@ -0,0 +1,4 @@
+export {
+  default as Notification,
+  type NotificationProps,
+} from './Notification';
--- practices/practice-two/src/main.tsx
+++ practices/practice-two/src/main.tsx
@@ -1,3 +1,4 @@
+import { Notification } from 'components';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -5,5 +6,12 @@ import ReactDOM from 'react-dom/client';
 import 'styles/index.module.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <div className="">
+      <Notification
+        title="Your actions executed successfully!"
+        message="A book has been removed from the system, this action can not undo."
+      />
+    </div>
+  </React.StrictMode>
 );
