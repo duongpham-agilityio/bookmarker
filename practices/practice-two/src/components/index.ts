889aad1e58e41405aaca9bce354dbd35374711fc
unknown <duong.pham@asnet.com.vn>
2023-03-30 23:50:19 +0700

    FIX: #36 Create Error component

--- practices/practice-two/src/components/index.ts
+++ practices/practice-two/src/components/index.ts
@@ -7,3 +7,4 @@ export { default as Form, type FormProps } from './Form';
 export { default as Popup, type PopupProps } from './Popup';
 export { default as Card, type CardProps } from './Card';
 export { default as CardSkeleton } from './Card/Skeleton';
+export { default as Error } from './Error';
--- practices/practice-two/src/constants/messages.ts
+++ practices/practice-two/src/constants/messages.ts
@@ -2,4 +2,6 @@
 export enum MESSAGES {
   EMPTY_TITLE = 'Oops! Nothing match with your keyword!',
   EMPTY_DESCRIPTION = 'You can search with other keyword or add another new book',
+  ERROR_TITLE = 'Oops! Something went wrong',
+  ERROR_DESCRIPTION = 'Please, help me reload pages.',
 }
