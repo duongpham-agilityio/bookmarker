a738ac7df4ec1ea29792469210b75ba97d151928
unknown <duong.pham@asnet.com.vn>
2023-03-30 10:23:28 +0700

    FIX: #36 Write unit test

--- practices/practice-two/src/main.tsx
+++ practices/practice-two/src/main.tsx
@@ -1,50 +1,9 @@
-import { Card } from 'components';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 
 // Styles
 import 'styles/index.module.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <div
-        className=""
-        style={{
-          width: '1440px',
-          display: 'grid',
-          gridTemplateColumns: '1fr 1fr 1fr',
-          gap: '22px',
-          margin: '50px auto',
-        }}
-      >
-        <Card
-          title="HTML/CSS Ebook HTML/CSS Ebook HTML/CSS Ebook HTML/CSS Ebook"
-          description="Description of some
-        book will displayed here Description of some
-        book will displayed here Description of some
-        book will displayed here"
-          publishedDate="9:00 AM"
-          imageUrl=""
-        />
-
-        <Card
-          title="HTML/CSS Ebook HTML/CSS Ebook HTML/CSS Ebook HTML/CSS Ebook"
-          description="Description of some
-        book will displayed here"
-          publishedDate="9:00 AM"
-          imageUrl=""
-        />
-
-        <Card
-          title="HTML/CSS Ebook HTML/CSS Ebook HTML/CSS Ebook HTML/CSS Ebook"
-          description="Description of some
-        book will displayed here"
-          publishedDate="9:00 AM"
-          imageUrl=""
-        />
-      </div>
-    </BrowserRouter>
-  </React.StrictMode>
+  <React.StrictMode></React.StrictMode>
 );
